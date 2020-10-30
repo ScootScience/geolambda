e68918d4c34f42e79b85f926876789a95ab6caa5
Connor Dibble <cddibble@gmail.com>
2020-10-30 14:18:28 -0700

    removed steps related to gdal packages

--- bin/package.sh
+++ bin/package.sh
@@ -15,9 +15,9 @@ cp -P ${PREFIX}/lib64/libjpeg*.so* $DEPLOY_DIR/lib/
 strip $DEPLOY_DIR/lib/* || true
 
 # copy GDAL_DATA files over
-mkdir -p $DEPLOY_DIR/share
-rsync -ax $PREFIX/share/gdal $DEPLOY_DIR/share/
-rsync -ax $PREFIX/share/proj $DEPLOY_DIR/share/
+# mkdir -p $DEPLOY_DIR/share
+# rsync -ax $PREFIX/share/gdal $DEPLOY_DIR/share/
+# rsync -ax $PREFIX/share/proj $DEPLOY_DIR/share/
 
 # zip up deploy package
 cd $DEPLOY_DIR
