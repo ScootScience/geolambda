f9a5c4b8a15f159badf6f1f844d3bfe684d5d8c4
Connor Dibble <cddibble@gmail.com>
2020-10-30 16:36:04 -0700

    no more xarray install

--- bin/package.sh
+++ bin/package.sh
@@ -21,7 +21,7 @@ mkdir -p $DEPLOY_DIR/python
 # rsync -ax $PREFIX/share/proj $DEPLOY_DIR/share/
 
 # copy python packages
-pip3 install cfgrib xarray -t $DEPLOY_DIR/python
+pip3 install cfgrib -t $DEPLOY_DIR/python
 
 # zip up deploy package
 cd $DEPLOY_DIR
