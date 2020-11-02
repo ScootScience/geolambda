ca602f7a55f4e966cca88e60e705068cd2b8dc71
Connor Dibble <cddibble@gmail.com>
2020-11-01 21:44:08 -0800

    trying to copy appropriate folders

--- bin/package.sh
+++ bin/package.sh
@@ -6,19 +6,20 @@ export DEPLOY_DIR=lambda
 echo Creating deploy package
 
 # make deployment directory and add lambda handler
-# mkdir -p $DEPLOY_DIR/lib
+mkdir -p $DEPLOY_DIR/lib
 mkdir -p $DEPLOY_DIR/python
 
 # copy libs
-# cp -P ${PREFIX}/lib/*.so* $DEPLOY_DIR/lib/
+cp -P ${PREFIX}/lib/*.so* $DEPLOY_DIR/lib/
 # cp -P ${PREFIX}/lib64/libjpeg*.so* $DEPLOY_DIR/lib/
 
 # strip $DEPLOY_DIR/lib/* || true
 
 # copy GDAL_DATA files over
-# mkdir -p $DEPLOY_DIR/share
+mkdir -p $DEPLOY_DIR/share
 # rsync -ax $PREFIX/share/gdal $DEPLOY_DIR/share/
 # rsync -ax $PREFIX/share/proj $DEPLOY_DIR/share/
+rsync -ax $PREFIX/eccodes/* $DEPLOY_DIR/share/
 
 # copy python packages
 pip3 install cfgrib pyeccodes -t $DEPLOY_DIR/python
