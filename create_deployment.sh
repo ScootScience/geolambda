8d6ae8d1301162458e72bbe7c9aa1cbde6fce6a2
Connor Dibble <cddibble@gmail.com>
2020-11-01 22:48:44 -0800

    still messing with directory sturcutre

--- create_deployment.sh
+++ create_deployment.sh
@@ -4,10 +4,10 @@
 echo "Creating deployment package"
 rsync -ax /lambda/* /deployment/python
 rsync -ax /lambda_root/* /deployment/python
-rsync -ax /usr/local/share/eccodes/definitions /deployment/python
-cd /deployment/python
-chmod 644 $(find /deployment/python -type f)
-chmod 755 $(find /deployment/python -type d)
+rsync -ax /usr/local/share/eccodes/definitions /deployment
+cd /deployment
+chmod 644 $(find /deployment -type f)
+chmod 755 $(find /deployment -type d)
 echo "Zipping..."
 ls /export
 zip -r9 /export/lambda.zip .
