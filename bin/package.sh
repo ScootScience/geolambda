ff8ad41f5f0042eb0da492c3471738042085d1ba
Connor Dibble <cddibble@gmail.com>
2020-11-02 22:33:29 -0800

    fixed specification for package

--- bin/package.sh
+++ bin/package.sh
@@ -23,7 +23,7 @@ mkdir -p $DEPLOY_DIR/share
 cp -P $PREFIX/eccodes/* $DEPLOY_DIR/share/
 
 # copy python packages
-pip3 install cfgrib pyeccodes eccodes python-eccodes -t $DEPLOY_DIR/python
+pip3 install cfgrib pyeccodes eccodes eccodes-python -t $DEPLOY_DIR/python
 
 # zip up deploy package
 cd $DEPLOY_DIR
